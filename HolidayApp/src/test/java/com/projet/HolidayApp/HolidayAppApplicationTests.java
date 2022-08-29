e843126b85b6038fd3e147c23edd27132286dcd0
ayoub_bsl <bsl.ayoub20@gmail.com>
2022-08-29 18:23:07 +0200

    init project

---
+++ HolidayApp/src/test/java/com/projet/HolidayApp/HolidayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.HolidayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
